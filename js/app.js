f6fb3177a918242966822c1db1e3973bb7985fb5
sauravsarkar100 <70365386+sauravsarkar100@users.noreply.github.com>
2020-09-14 23:18:50 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+
+let name = prompt("what is your name?");
+let sechen = prompt("what is your patner name?");
+let number = Math.random()*100;
+number = Math.floor(number)+1;
+alert(name  + " and " + sechen + "'s love percentage is: " + number + "%");
